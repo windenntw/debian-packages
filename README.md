19cd746c7fa63aee463ecc0ef05c05cb2ad9a45d
winden <windenntw@gmail.com>
2026-05-25 16:56:44 +0200

    Binaries and sources.

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ To enable a debian machine to use this local package repo:
 
 ```
 $ cat <<EOF >/etc/apt/sources.list.d/windenntw-debian-packages.list 
-deb [trusted=yes] http://windenntw.github.io/debian-packages binaries/
+deb     [trusted=yes] http://windenntw.github.io/debian-packages binaries/
+deb-src [trusted=yes] http://windenntw.github.io/debian-packages source/
 EOF
 ```
 
--- make-indexes.sh
+++ make-indexes.sh
@@ -1,1 +1,49 @@
-dpkg-scanpackages binaries /dev/null | gzip -9c > binaries/Packages.gz
+#!/bin/bash
+
+# Function to generate indexes and Release file for a given directory
+generate_repo_info() {
+    local dir_name="$1"
+
+    local scan_cmd
+    local index_name
+    local arch
+
+    if [ "$dir_name" = "binaries" ]; then
+        scan_cmd="dpkg-scanpackages"
+        index_name="Packages.gz"
+        arch="amd64"
+    elif [ "$dir_name" = "source" ]; then
+        scan_cmd="dpkg-scansources"
+        index_name="Sources.gz"
+        arch="source"
+    else
+        echo "Error: Unknown type '$dir_name'" >&2
+        return 1
+    fi
+
+    echo "Generating index for $dir_name..."
+
+    # 1. Generate only the compressed list
+    "$scan_cmd" "$dir_name" /dev/null | gzip -9c > "$dir_name/$index_name"
+
+    # 2. Generate Release file header
+    cat <<EOF > "$dir_name/Release"
+Origin: windenntw
+Label: windenntw
+Suite: windenntw
+Codename: windenntw
+Architectures: $arch
+Date: $(date -Ru)
+SHA256:
+EOF
+
+    # 3. Append only index hash and size
+    (
+        cd "$dir_name" || exit 1
+        echo " $(sha256sum "$index_name" | cut -d' ' -f1) $(stat -c%s "$index_name") $index_name" >> Release
+    )
+}
+
+# Generate info for both directories
+generate_repo_info binaries
+generate_repo_info source
