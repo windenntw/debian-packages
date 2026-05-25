fd885ab4261f218a72a9efbb0ed6ae4a32d6c8f9
winden <windenntw@gmail.com>
2026-05-25 17:34:15 +0200

    X

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ To enable a debian machine to use this local package repo:
 
 ```bash
 cat <<EOF >/etc/apt/sources.list.d/windenntw-debian-packages.list 
-deb     [trusted=yes] https://windenntw.github.io/debian-packages binaries/
-deb-src [trusted=yes] https://windenntw.github.io/debian-packages source/
+deb     [trusted=yes] https://windenntw.github.io/debian-packages windenntw windenntw
+deb-src [trusted=yes] https://windenntw.github.io/debian-packages windenntw windenntw
 EOF
 ```
 
--- make-indexes.sh
+++ make-indexes.sh
@@ -2,4 +2,23 @@
 
 set -x +
 
-# TODO use apt-ftparchive to generate the corresponding files.
+generate_index() {
+    local index_type="$1"
+    local dir_path="$2"
+    local output_file="$3"
+
+    echo "Generating ${output_file}..."
+    apt-ftparchive "${index_type}" "${dir_path}" \
+    | tee "${dir_path}/${output_file}" \
+    | gzip -9c > "${dir_path}/${output_file}.gz"
+}
+
+# Generate Packages and Sources
+generate_index packages dists/windenntw/windenntw/binary-amd64 Packages
+generate_index sources dists/windenntw/windenntw/source Sources
+
+echo "Generating Release file..."
+# Generate the top-level Release file
+apt-ftparchive -c apt-release.conf release dists/windenntw > dists/windenntw/Release
+
+echo "Done!"
